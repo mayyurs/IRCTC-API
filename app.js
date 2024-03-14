39573d5e6e8d6150fca138e3b2e598978cc9e3fb
Mayur Shinde <110163825+mayyurs@users.noreply.github.com>
2024-03-14 21:50:49 +0530

    Updated app.js with booking seats and get booking details

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const mysql = require('mysql2')
+
 const app = express();
 const port = 3000;
 
@@ -93,7 +94,6 @@ app.post('/api/login', (req, res) => {
     });
   });
 });
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJpYXQiOjE3MTA0MTM3NzYsImV4cCI6MTcxMDQxNzM3Nn0.G_cS-aJTzoN4p-A7Suzp9318GfGVXG1bcOZu6C1heaQ
 
 // Endpoint for adding a new train
 app.post('/api/trains/create', verifyToken, (req, res) => {
@@ -149,95 +149,93 @@ app.get('/api/trains/availability', (req, res) => {
 
 // Endpoint to book a train ticket
 app.post('/api/trains/:train_id/book', verifyToken, (req, res) => {
-    const { user_id, no_of_seats } = req.body;
-    const train_id = req.params.train_id;
-    
-    if (!user_id || !no_of_seats) {
-      return res.status(400).json({ error: 'Missing user_id or no_of_seats in request body' });
-    }
+  const { user_id, no_of_seats } = req.body;
+  const train_id = req.params.train_id;
   
-    pool.getConnection((err, connection) => {
-      if (err) {
-        console.error('Error getting MySQL connection', err);
+  if (!user_id || !no_of_seats) {
+    return res.status(400).json({ error: 'Missing user_id or no_of_seats in request body' });
+  }
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error getting MySQL connection', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    connection.query('SELECT seat_capacity - booked_seats AS available_seats FROM trains WHERE id = ?', [train_id], (error, results, fields) => {
+      if (error) {
+        console.error('Error executing MySQL query', error);
+        connection.release();
         return res.status(500).json({ error: 'Internal server error' });
       }
-  
-      // Begin transaction
-      connection.beginTransaction((error) => {
-        if (error) {
-          console.error('Error beginning MySQL transaction', error);
-          connection.release();
-          return res.status(500).json({ error: 'Internal server error' });
-        }
-  
-        // Check available seats
-        connection.query('SELECT seat_capacity - booked_seats AS available_seats FROM trains WHERE id = ?', [train_id], (error, results, fields) => {
+
+      const available_seats = results[0].available_seats;
+
+      if (no_of_seats > available_seats) {
+        connection.release();
+        return res.status(400).json({ error: 'Not enough available seats' });
+      }
+
+      const bookingIds = [];
+      const seatNumbers = [];
+      let insertedCount = 0; // Track the number of successful insertions
+      for (let i = 0; i < no_of_seats; i++) {
+        connection.query('INSERT INTO bookings (train_id, user_id, no_of_seats, seat_numbers) VALUES (?, ?, ?, ?)', [parseInt(train_id), parseInt(user_id), parseInt(no_of_seats), 1 ], (error, results, fields) => {
           if (error) {
             console.error('Error executing MySQL query', error);
-            connection.rollback(() => {
-              connection.release();
-              return res.status(500).json({ error: 'Internal server error' });
-            });
-          }
-  
-          const available_seats = results[0].available_seats;
-  
-          if (no_of_seats > available_seats) {
-            connection.rollback(() => {
-              connection.release();
-              return res.status(400).json({ error: 'Not enough available seats' });
-            });
-          }
-  
-          // Book seats
-          const bookingIds = [];
-          const seatNumbers = [];
-          let insertedCount = 0; // Track the number of successful insertions
-          for (let i = 0; i < no_of_seats; i++) {
-            connection.query('INSERT INTO bookings (train_id, user_id) VALUES (?, ?)', [train_id, user_id], (error, results, fields) => {
-              if (error) {
-                console.error('Error executing MySQL query', error);
-                connection.rollback(() => {
+            connection.release();
+            return res.status(500).json({ error: 'Internal server error' });
+          } else {
+            insertedCount++;
+            bookingIds.push(results.insertId);
+            seatNumbers.push(i + 1);
+
+            if (insertedCount === no_of_seats) {
+              connection.query('UPDATE trains SET booked_seats = booked_seats + ? WHERE id = ?', [no_of_seats, train_id], (error, results, fields) => {
+                if (error) {
+                  console.error('Error executing MySQL query', error);
                   connection.release();
                   return res.status(500).json({ error: 'Internal server error' });
-                });
-              } else {
-                insertedCount++;
-                bookingIds.push(results.insertId);
-                seatNumbers.push(i + 1);
-  
-                // If all seats have been successfully inserted, update booked_seats and commit the transaction
-                if (insertedCount === no_of_seats) {
-                  connection.query('UPDATE trains SET booked_seats = booked_seats + ? WHERE id = ?', [no_of_seats, train_id], (error, results, fields) => {
-                    if (error) {
-                      console.error('Error executing MySQL query', error);
-                      connection.rollback(() => {
-                        connection.release();
-                        return res.status(500).json({ error: 'Internal server error' });
-                      });
-                    }
-  
-                    // Commit transaction
-                    connection.commit((error) => {
-                      if (error) {
-                        console.error('Error committing MySQL transaction', error);
-                        connection.rollback(() => {
-                          connection.release();
-                          return res.status(500).json({ error: 'Internal server error' });
-                        });
-                      }
-                      connection.release();
-                      res.status(200).json({ message: 'Seat(s) booked successfully', booking_ids: bookingIds, seat_numbers: seatNumbers });
-                    });
-                  });
                 }
-              }
-            });
+
+                connection.commit((error) => {
+                  if (error) {
+                    console.error('Error committing MySQL transaction', error);
+                    connection.rollback(() => {
+                      connection.release();
+                      return res.status(500).json({ error: 'Internal server error' });
+                    });
+                  }
+                  connection.release();
+                  res.status(200).json({ message: 'Seat(s) booked successfully', booking_ids: bookingIds, seat_numbers: seatNumbers });
+                });
+              });
+            }
           }
         });
-      });
+      }
     });
   });
+});
+
+// Endpoint for getting specific booking details
+app.get('/api/bookings/:booking_id', verifyToken, async (req, res) => {
+  const bookingId = req.params.booking_id;
+
+  try {
+    const [rows] = await pool.promise().query('SELECT * FROM bookings WHERE id = ?', [bookingId]);
+
+    if (rows.length > 0) {
+      res.status(200).json(rows[0]);
+    } else {
+      res.status(404).json({ error: 'Booking not found' });
+    }
+  } catch (error) {
+    console.error('Error retrieving booking details:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
